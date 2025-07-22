9fbc3ebc5bc1777ac6a2ced4c38ad8a7e9614282
Louis-Ferrari <louis.ferrari@epitech.eu>
2025-07-22 16:34:55 +0200

    refactor(config): :art: use of a global namespace instead of a ranged one

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,17 +1,16 @@
 using Godot;
 
-namespace AshesofVelsingrad
+namespace AshesofVelsingrad;
+
+/// <summary>
+/// Classe principale du projet Godot. Gère la scène principale et les interactions globales.
+/// </summary>
+public partial class Main : Node
 {
-	/// <summary>
-	/// Classe principale du projet Godot. Gère la scène principale et les interactions globales.
-	/// </summary>
-	public partial class Main : Node
-	{
-		/// <summary>
-		/// Incrémente la valeur passée en paramètre de 1.
-		/// </summary>
-		/// <param name="value">La valeur à incrémenter.</param>
-		/// <returns>La valeur incrémentée de 1.</returns>
-		public int TempCounter(int value) => value + 1;
-	}
+    /// <summary>
+    /// Incrémente la valeur passée en paramètre de 1.
+    /// </summary>
+    /// <param name="value">La valeur à incrémenter.</param>
+    /// <returns>La valeur incrémentée de 1.</returns>
+    public int TempCounter(int value) => value + 1;
 }

07f8b948961d0ef20ba265ccad5c8a84c967a4da
Louis-Ferrari <louis.ferrari@epitech.eu>
2025-07-28 16:18:23 +0200

    feat(ui): :sparkles: add exit button to leave the game

--- scripts/menu/MainMenu.cs
+++ scripts/menu/MainMenu.cs
@@ -6,11 +6,18 @@ public partial class MainMenu : Control
     public override void _Ready()
     {
         GetNode<Button>("OptionsButton").ButtonUp += OnOptionsButtonButtonUp;
+        GetNode<Button>("ExitButton").ButtonUp += OnExitButtonButtonUp;
     }
 
     private void OnOptionsButtonButtonUp()
     {
         GD.Print("LOAD OPTIONS MENU");
         GetTree().ChangeSceneToFile("res://scenes/menu/options_menu.tscn");
+    }
+
+    private void OnExitButtonButtonUp()
+    {
+        GD.Print("EXIT GAME");
+        GetTree().Quit();
     }
 }

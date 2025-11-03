e15d7f0e30abbc42036adffc313555b9954fc5be
XEric <eric1.xu@epitech.eu>
2025-11-03 23:39:14 +0100

    chore: push godot internal modification

--- scripts/data/Enemy1Data.cs
+++ scripts/data/Enemy1Data.cs
@@ -5,18 +5,18 @@ namespace AshesOfVelsingrad;
 
 public sealed partial class Enemy1Data : UnitSystem
 {
-    protected override void Initialize()
-    {
-        UnitName = "Enemy1";
-        BaseSpeed = 300;
-    }
+	protected override void Initialize()
+	{
+		UnitName = "Enemy1";
+		BaseSpeed = 300;
+	}
 
-    public override void Attack(List<UnitSystem> targets, MapSystem? map)
-    {
-        ReportSystemUnitHasPlayed();
-    }
+	public override void Attack(List<UnitSystem> targets, MapSystem? map)
+	{
+		ReportSystemUnitHasPlayed();
+	}
 
-    public override void TakeDamage(float damage)
-    {
-    }
+	public override void TakeDamage(float damage)
+	{
+	}
 }

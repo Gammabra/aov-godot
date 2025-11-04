1e42b7c90022faac1d0c15b1ac45e005080b2979
XEric <eric1.xu@epitech.eu>
2025-11-04 15:19:05 +0100

    chore: attribute basic values to the test units

--- scripts/data/Enemy1Data.cs
+++ scripts/data/Enemy1Data.cs
@@ -8,7 +8,18 @@ public sealed partial class Enemy1Data : UnitSystem
 	protected override void Initialize()
 	{
 		UnitName = "Enemy1";
-		BaseSpeed = 300;
+		Description = "Test enemy unit";
+		MaxHp = 1000;
+		Hp = MaxHp;
+		BaseAtk = 100;
+		BaseDef = 100;
+		BaseSpeed = 100;
+		Intelligence = 100;
+		ManaPoint = 100;
+		IsAlive = true;
+		HasPlayed = false;
+		PossibleMovesRange = 2;
+		Curse = 0;
 	}
 
 	public override void Attack(List<UnitSystem> targets, MapSystem? map)
--- scripts/data/Player1Data.cs
+++ scripts/data/Player1Data.cs
@@ -8,7 +8,18 @@ public sealed partial class Player1Data : UnitSystem
 	protected override void Initialize()
 	{
 		UnitName = "Player1";
+		Description = "Test player unit";
+		MaxHp = 2000;
+		Hp = MaxHp;
+		BaseAtk = 200;
+		BaseDef = 200;
 		BaseSpeed = 200;
+		Intelligence = 200;
+		ManaPoint = 200;
+		IsAlive = true;
+		HasPlayed = false;
+		PossibleMovesRange = 2;
+		Curse = 0;
 	}
 
 	public override void Attack(List<UnitSystem> targets, MapSystem? map)

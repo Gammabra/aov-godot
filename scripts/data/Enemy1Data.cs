aa06d67ecebbbabfb3108fef77da22339c0b81a6
XEric <eric1.xu@epitech.eu>
2025-11-09 14:32:54 +0100

    fix: update test units

--- scripts/data/Enemy1Data.cs
+++ scripts/data/Enemy1Data.cs
@@ -5,29 +5,20 @@ namespace AshesOfVelsingrad;
 
 public sealed partial class Enemy1Data : UnitSystem
 {
-	protected override void Initialize()
-	{
-		UnitName = "Enemy1";
-		Description = "Test enemy unit";
-		MaxHp = 1000;
-		Hp = MaxHp;
-		BaseAtk = 100;
-		BaseDef = 100;
-		BaseSpeed = 100;
-		Intelligence = 100;
-		ManaPoint = 100;
-		IsAlive = true;
-		HasPlayed = false;
-		PossibleMovesRange = 2;
-		Curse = 0;
-	}
-
-	public override void Attack(List<UnitSystem> targets, MapSystem? map)
-	{
-		ReportSystemUnitHasPlayed();
-	}
-
-	public override void TakeDamage(float damage)
-	{
-	}
+    protected override void Initialize()
+    {
+        UnitName = "Enemy1";
+        Description = "Test enemy unit";
+        MaxHp = 1000;
+        Hp = MaxHp;
+        BaseAtk = 100;
+        BaseDef = 100;
+        BaseSpeed = 100;
+        Intelligence = 100;
+        ManaPoint = 100;
+        IsAlive = true;
+        HasPlayed = false;
+        PossibleMovesRange = 2;
+        Curse = 0;
+    }
 }
--- scripts/data/Player1Data.cs
+++ scripts/data/Player1Data.cs
@@ -5,29 +5,20 @@ namespace AshesOfVelsingrad;
 
 public sealed partial class Player1Data : UnitSystem
 {
-	protected override void Initialize()
-	{
-		UnitName = "Player1";
-		Description = "Test player unit";
-		MaxHp = 2000;
-		Hp = MaxHp;
-		BaseAtk = 200;
-		BaseDef = 200;
-		BaseSpeed = 200;
-		Intelligence = 200;
-		ManaPoint = 200;
-		IsAlive = true;
-		HasPlayed = false;
-		PossibleMovesRange = 2;
-		Curse = 0;
-	}
-
-	public override void Attack(List<UnitSystem> targets, MapSystem? map)
-	{
-		ReportSystemUnitHasPlayed();
-	}
-
-	public override void TakeDamage(float damage)
-	{
-	}
+    protected override void Initialize()
+    {
+        UnitName = "Player1";
+        Description = "Test player unit";
+        MaxHp = 2000;
+        Hp = MaxHp;
+        BaseAtk = 200;
+        BaseDef = 200;
+        BaseSpeed = 200;
+        Intelligence = 200;
+        ManaPoint = 200;
+        IsAlive = true;
+        HasPlayed = false;
+        PossibleMovesRange = 2;
+        Curse = 0;
+    }
 }

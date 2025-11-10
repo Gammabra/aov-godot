a79bd4774d92007c5dcbb0030301bf615d51d8f4
XEric <eric1.xu@epitech.eu>
2025-11-10 22:07:20 +0100

    feat: add active skills to the test unit

--- scripts/data/Player1Data.cs
+++ scripts/data/Player1Data.cs
@@ -3,6 +3,71 @@ using AshesOfVelsingrad.systems;
 
 namespace AshesOfVelsingrad;
 
+public sealed class Skill1 : SkillSystem
+{
+    public Skill1()
+    {
+        TargetType = TargetTypes.SingleEnemy;
+    }
+
+    public override void Use(List<UnitSystem> targets, MapSystem? map)
+    {
+        targets[0].TakeDamage(1);
+    }
+}
+
+public sealed class Skill2 : SkillSystem
+{
+    public Skill2()
+    {
+        TargetType = TargetTypes.SingleEnemy;
+    }
+
+    public override void Use(List<UnitSystem> targets, MapSystem? map)
+    {
+        targets[0].TakeDamage(2);
+    }
+}
+
+public sealed class Skill3 : SkillSystem
+{
+    public Skill3()
+    {
+        TargetType = TargetTypes.SingleEnemy;
+    }
+
+    public override void Use(List<UnitSystem> targets, MapSystem? map)
+    {
+        targets[0].TakeDamage(3);
+    }
+}
+
+public sealed class Skill4 : SkillSystem
+{
+    public Skill4()
+    {
+        TargetType = TargetTypes.SingleEnemy;
+    }
+
+    public override void Use(List<UnitSystem> targets, MapSystem? map)
+    {
+        targets[0].TakeDamage(4);
+    }
+}
+
+public sealed class Skill5 : SkillSystem
+{
+    public Skill5()
+    {
+        TargetType = TargetTypes.SingleEnemy;
+    }
+
+    public override void Use(List<UnitSystem> targets, MapSystem? map)
+    {
+        targets[0].TakeDamage(5);
+    }
+}
+
 public sealed partial class Player1Data : UnitSystem
 {
     protected override void Initialize()
@@ -20,5 +85,10 @@ public sealed partial class Player1Data : UnitSystem
         HasPlayed = false;
         PossibleMovesRange = 2;
         Curse = 0;
+        ActiveSkills.Add(new Skill1());
+        ActiveSkills.Add(new Skill2());
+        ActiveSkills.Add(new Skill3());
+        ActiveSkills.Add(new Skill4());
+        ActiveSkills.Add(new Skill5());
     }
 }

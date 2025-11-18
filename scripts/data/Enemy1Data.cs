21f83dcd7fa42c07a9f6c5c858bd54a059d10af7
XEric <eric1.xu@epitech.eu>
2025-11-18 02:08:18 +0100

    fix: HasPlayed doesn't exists anymore

--- scripts/data/Enemy1Data.cs
+++ scripts/data/Enemy1Data.cs
@@ -8,7 +8,7 @@ public sealed partial class Enemy1Data : UnitSystem
     {
         UnitName = "Enemy1";
         Description = "Test enemy unit";
-        MaxHp = 1000;
+        MaxHp = 2;
         Hp = MaxHp;
         BaseAtk = 100;
         BaseDef = 100;
@@ -16,7 +16,6 @@ public sealed partial class Enemy1Data : UnitSystem
         Intelligence = 100;
         ManaPoint = 100;
         IsAlive = true;
-        HasPlayed = false;
         PossibleMovesRange = 2;
         Curse = 0;
     }

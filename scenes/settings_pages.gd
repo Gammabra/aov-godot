eb383ab9fe39df1e626d22a1c3d11a983a81a5e6
Baffreaua <amaury.baffreau@epitech.eu>
2025-11-20 13:36:27 +0900

    feat: different category selection in the settings using the buttons

--- scenes/settings_pages.gd
+++ scenes/settings_pages.gd
@@ -1,22 +1,23 @@
 extends Node
 
 func show_page(page_name):
-	print("Changement de page vers :", page_name)
 	for p in get_children():
 		p.visible = false
+
 	get_node(page_name).visible = true
+
 
 func _on_button_subtitle_pressed():
 	show_page("PageSubtitle")
 
-func _on_button_video_pressed() -> void:
+func _on_button_video_pressed():
 	show_page("PageVideo")
 
-func _on_button_visual_pressed() -> void:
+func _on_button_visual_pressed():
 	show_page("PageVisual")
 
-func _on_button_command_pressed() -> void:
-	show_page("PageCom")
+func _on_button_command_pressed():
+	show_page("PageCommand")
 
-func _on_button_audio_pressed() -> void:
+func _on_button_audio_pressed():
 	show_page("PageAudio")

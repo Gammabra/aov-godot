53dd23cc9606e6f08f9d407cf1fd254e33f37585
XEric <eric1.xu@epitech.eu>
2025-12-19 16:35:00 +0100

    fix(tests): fix already existing tests and add more tests for the GameManagerUtils file functions

--- tests/unit/managers/GameManagerTest.cs
+++ tests/unit/managers/GameManagerTest.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using AshesOfVelsingrad.Managers;
 using AshesOfVelsingrad.Systems;
 using GdUnit4;
 using Godot;
-using Godot.Collections;
 using static GdUnit4.Assertions;
 
 namespace UnitTests;
@@ -38,7 +36,7 @@ public class GameManagerTest
             .SetValue(null, null);
     }
 
-    private TestConcreteUnitSystem CreateUnit(string name, int hp = 100)
+    private TestConcreteUnitSystem CreateUnit(string name, float hp = 100)
     {
         TestConcreteUnitSystem unit = new(hp: hp);
         unit.Name = name;
@@ -75,6 +73,8 @@ public class GameManagerTest
     [TestCase]
     public void Initialize_SetsSingleton()
     {
+        GD.Print("[TEST] Start Initialize_SetsSingleton");
+
         GameManager manager = AddNode(new GameManager());
         manager.Call("Initialize");
 
@@ -89,6 +89,8 @@ public class GameManagerTest
     [TestCase]
     public void LoadUnits_AddsUnitsFromContainers()
     {
+        GD.Print("[TEST] Start LoadUnits_AddsUnitsFromContainers");
+
         GameManager manager = AddNode(new GameManager());
 
         Node playerContainer = AddNode(new Node());
@@ -105,73 +107,224 @@ public class GameManagerTest
 
         manager.Call("LoadUnits");
 
-        object playerUnitsObj = manager.Get("_playerUnits");
-        object enemyUnitsObj = manager.Get("_enemyUnits");
-
-        List<UnitSystem>? playerUnits = playerUnitsObj as List<UnitSystem>;
-        List<UnitSystem>? enemyUnits = enemyUnitsObj as List<UnitSystem>;
-
-        if (playerUnits is null || enemyUnits is null)
-            return;
-        AssertThat(playerUnits.Count).IsEqual(1);
-        AssertThat(enemyUnits.Count).IsEqual(1);
+        AssertThat(playerContainer.GetChildCount()).IsEqual(1);
+        AssertThat(playerContainer.GetChildCount()).IsEqual(1);
+        AssertThat(GetPrivateField<List<UnitSystem>>(manager, "_playerUnits").Count).IsEqual(1);
+        AssertThat(GetPrivateField<List<UnitSystem>>(manager, "_enemyUnits").Count).IsEqual(1);
     }
 
     [TestCase]
     public void CheckWinLoseCondition_SetsVictory_WhenNoEnemiesAlive()
     {
-        GameManager manager = AddNode(new GameManager());
-        SetPrivateField(manager, "_gameOutcome", GameOutcome.Ongoing);
-        TurnManager turnManager = new();
-        manager.Set("_turnManagerContainer", turnManager);
+        GD.Print("[TEST] Start CheckWinLoseCondition_SetsVictory_WhenNoEnemiesAlive");
 
+        GameManager manager = AddNode(new GameManager());
+        Node playerContainer = AddNode(new Node());
+        Node enemyContainer = AddNode(new Node());
         TestConcreteUnitSystem playerUnit = CreateUnit("Player");
         TestConcreteUnitSystem enemyUnit = CreateUnit("Enemy", 0);
+
         enemyUnit.SetIsAlive(false);
 
-        List<UnitSystem> playerUnits = [];
-        playerUnits.Add(playerUnit);
-        List<UnitSystem> enemyUnits = [];
-        enemyUnits.Add(enemyUnit);
+        SetPrivateField(manager, "_gameOutcome", GameOutcome.Ongoing);
+        playerContainer.AddChild(playerUnit);
+        enemyContainer.AddChild(enemyUnit);
 
-        SetPrivateField(manager, "_playerUnits", playerUnits);
-        SetPrivateField(manager, "_enemyUnits", enemyUnits);
+        SetPrivateField(manager, "_playerUnitsContainer", playerContainer);
+        SetPrivateField(manager, "_enemyUnitsContainer", enemyContainer);
+
+        CallPrivateMethod(manager, "LoadUnits");
+
         CallPrivateMethod(manager, "CheckWinLoseCondition");
 
-        Variant outcome = manager.Get("_gameOutcome");
-        AssertThat((int)outcome).IsEqual((int)GameOutcome.Victory);
+        GameOutcome outcome = GetPrivateField<GameOutcome>(manager, "_gameOutcome");
+        AssertThat(GetPrivateField<List<UnitSystem>>(manager, "_playerUnits")[0].IsAlive).IsTrue();
+        AssertThat(GetPrivateField<List<UnitSystem>>(manager, "_enemyUnits")[0].IsAlive).IsFalse();
+        AssertThat(outcome).IsEqual(GameOutcome.Victory);
     }
 
     [TestCase]
     public void CheckWinLoseCondition_SetsDefeat_WhenNoPlayersAlive()
     {
-        GameManager manager = AddNode(new GameManager());
-        SetPrivateField(manager, "_gameOutcome", GameOutcome.Ongoing);
-        TurnManager turnManager = new();
-        manager.Set("_turnManagerContainer", turnManager);
+        GD.Print("[TEST] Start CheckWinLoseCondition_SetsDefeat_WhenNoPlayersAlive");
 
+        GameManager manager = AddNode(new GameManager());
+        Node playerContainer = AddNode(new Node());
+        Node enemyContainer = AddNode(new Node());
         TestConcreteUnitSystem playerUnit = CreateUnit("Player", 0);
-        playerUnit.SetIsAlive(false);
         TestConcreteUnitSystem enemyUnit = CreateUnit("Enemy");
 
-        List<UnitSystem> playerUnits = [];
-        playerUnits.Add(playerUnit);
-        List<UnitSystem> enemyUnits = [];
-        enemyUnits.Add(enemyUnit);
+        playerUnit.SetIsAlive(false);
 
-        SetPrivateField(manager, "_playerUnits", playerUnits);
-        SetPrivateField(manager, "_enemyUnits", enemyUnits);
+        SetPrivateField(manager, "_gameOutcome", GameOutcome.Ongoing);
+        playerContainer.AddChild(playerUnit);
+        enemyContainer.AddChild(enemyUnit);
+
+        SetPrivateField(manager, "_playerUnitsContainer", playerContainer);
+        SetPrivateField(manager, "_enemyUnitsContainer", enemyContainer);
+
+        CallPrivateMethod(manager, "LoadUnits");
+
         CallPrivateMethod(manager, "CheckWinLoseCondition");
 
-        Variant outcome = manager.Get("_gameOutcome");
+        GameOutcome outcome = GetPrivateField<GameOutcome>(manager, "_gameOutcome");
+        AssertThat(GetPrivateField<List<UnitSystem>>(manager, "_playerUnits")[0].IsAlive).IsFalse();
+        AssertThat(GetPrivateField<List<UnitSystem>>(manager, "_enemyUnits")[0].IsAlive).IsTrue();
         AssertThat((int)outcome).IsEqual((int)GameOutcome.Defeat);
+    }
+
+    [TestCase]
+    public void CheckUnitsLife_SetsAllUnitsDead()
+    {
+        GD.Print("[TEST] Start CheckUnitsLife_SetsAllUnitsDead");
+
+        GameManager manager = AddNode(new GameManager());
+        Node playerContainer = AddNode(new Node());
+        Node enemyContainer = AddNode(new Node());
+        TestConcreteUnitSystem playerUnit = CreateUnit("Player", 0);
+        TestConcreteUnitSystem enemyUnit = CreateUnit("Enemy", 0);
+
+        SetPrivateField(manager, "_gameOutcome", GameOutcome.Ongoing);
+        playerContainer.AddChild(playerUnit);
+        enemyContainer.AddChild(enemyUnit);
+
+        SetPrivateField(manager, "_playerUnitsContainer", playerContainer);
+        SetPrivateField(manager, "_enemyUnitsContainer", enemyContainer);
+
+        CallPrivateMethod(manager, "LoadUnits");
+
+        List<UnitSystem> playerUnits = GetPrivateField<List<UnitSystem>>(manager, "_playerUnits");
+        List<UnitSystem> enemyUnits = GetPrivateField<List<UnitSystem>>(manager, "_enemyUnits");
+
+        List<UnitSystem> units = playerUnits;
+        units.AddRange(enemyUnits);
+
+        CallPrivateMethod(manager, "CheckUnitsLife", units);
+
+        AssertThat(playerUnit.IsAlive).IsFalse();
+        AssertThat(enemyUnit.IsAlive).IsFalse();
+    }
+
+    [TestCase]
+    public void CheckUnitTurnEnd_TriggersVictory()
+    {
+        GD.Print("[TEST] Start CheckUnitTurnEnd_TriggersVictory");
+
+        GameManager manager = AddNode(new GameManager());
+        Node playerContainer = AddNode(new Node());
+        Node enemyContainer = AddNode(new Node());
+        TestConcreteUnitSystem playerUnit = CreateUnit("Player");
+        TestConcreteUnitSystem enemyUnit = CreateUnit("Enemy", 0);
+
+        SetPrivateField(manager, "_gameOutcome", GameOutcome.Victory);
+        playerContainer.AddChild(playerUnit);
+        enemyContainer.AddChild(enemyUnit);
+
+        SetPrivateField(manager, "_playerUnitsContainer", playerContainer);
+        SetPrivateField(manager, "_enemyUnitsContainer", enemyContainer);
+
+        CallPrivateMethod(manager, "LoadUnits");
+
+        CallPrivateMethod(manager, "CheckUnitTurnEnd");
+
+        GameOutcome outcome = GetPrivateField<GameOutcome>(manager, "_gameOutcome");
+        AssertThat(GetPrivateField<List<UnitSystem>>(manager, "_playerUnits")[0].IsAlive).IsTrue();
+        AssertThat(GetPrivateField<List<UnitSystem>>(manager, "_enemyUnits")[0].IsAlive).IsFalse();
+        AssertThat(outcome).IsEqual(GameOutcome.Victory);
+    }
+
+    [TestCase]
+    public void HandlePlayerUnitMove_CellNotReachable_ReenablesInput()
+    {
+        GD.Print("[TEST] Start HandlePlayerUnitMove_CellNotReachable_ReenablesInput");
+
+        GameManager manager = AddNode(new GameManager());
+
+        BattleInputSystem inputSystem = AddNode(new BattleInputSystem());
+        manager.Set("_battleInputSystemContainer", inputSystem);
+
+        SetPrivateField(
+            manager,
+            "_currentUnitPossibleMoves",
+            new List<(int, int, int)>()
+        );
+
+        CallPrivateMethod(
+            manager,
+            "HandlePlayerUnitMove",
+            new Vector3I(1, 0, 1)
+        );
+
+        AssertThat(GetPrivateField<bool>(inputSystem, "_inputEnabled")).IsTrue();
+    }
+
+    [TestCase]
+    public void HandlePlayerUnitMove_ValidMove()
+    {
+        GD.Print("[TEST] Start HandlePlayerUnitMove_ValidMove_ClearsPossibleMoves");
+
+        GameManager manager = AddNode(new GameManager());
+        BattleInputSystem inputSystem = AddNode(new BattleInputSystem());
+        TestConcreteMapSystem mapSystem = AddNode(new TestConcreteMapSystem());
+
+        SetPrivateField(manager, "_battleInputSystemContainer", inputSystem);
+        SetPrivateField(manager, "_mapSystemContainer", mapSystem);
+
+        SetPrivateField(
+            manager,
+            "_currentUnitPossibleMoves",
+            new List<(int, int, int)> { (1, 0, 1) }
+        );
+
+        CallPrivateMethod(
+            manager,
+            "HandlePlayerUnitMove",
+            new Vector3I(1, 0, 1)
+        );
+
+        AssertThat(GetPrivateField<bool>(inputSystem, "_inputEnabled")).IsTrue();
+    }
+
+    [TestCase]
+    public void HandlePlayerSelectTarget_CellNotReachable_ReenablesInput()
+    {
+        GD.Print("[TEST] Start HandlePlayerSelectTarget_CellNotReachable_ReenablesInput");
+
+        GameManager manager = AddNode(new GameManager());
+        BattleInputSystem inputSystem = AddNode(new BattleInputSystem());
+        TestConcreteMapSystem mapSystem = AddNode(new TestConcreteMapSystem());
+
+        SetPrivateField(manager, "_battleInputSystemContainer", inputSystem);
+        SetPrivateField(manager, "_mapSystemContainer", mapSystem);
+
+        SetPrivateField(
+            manager,
+            "_currentUnitReachableCellsForCurrentSelectedSkill",
+            new List<(int, int, int)>()
+        );
+
+        CallPrivateMethod(
+            manager,
+            "HandlePlayerSelectTarget",
+            new Vector3I(2, 0, 2)
+        );
+
+        AssertThat(GetPrivateField<bool>(inputSystem, "_inputEnabled")).IsTrue();
     }
 
     #endregion
 
     #region Private Utilities
 
-    private void SetPrivateField(object obj, string field, object value)
+    private static T GetPrivateField<T>(object obj, string field)
+    {
+        return (T)obj
+            .GetType()
+            .GetField(field, BindingFlags.Instance | BindingFlags.NonPublic)!
+            .GetValue(obj)!;
+    }
+
+    private static void SetPrivateField(object obj, string field, object value)
     {
         obj
             .GetType()
@@ -179,7 +332,7 @@ public class GameManagerTest
             .SetValue(obj, value);
     }
 
-    private object? CallPrivateMethod(object obj, string methodName, params object?[] parameters)
+    private static object? CallPrivateMethod(object obj, string methodName, params object?[] parameters)
     {
         MethodInfo method = obj
                 .GetType()

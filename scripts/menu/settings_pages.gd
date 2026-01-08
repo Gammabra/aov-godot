3d512502d0dfb7ff33553ce3b4bf968faca4019e
Baffreaua <amaury.baffreau@epitech.eu>
2026-01-08 19:09:54 +0900

    feat: placement text settings + volume and resolution

--- scripts/menu/settings_pages.gd
+++ scripts/menu/settings_pages.gd
@@ -6,7 +6,6 @@ func show_page(page_name):
 
 	get_node(page_name).visible = true
 
-
 func _on_button_subtitle_pressed():
 	show_page("PageSubtitle")
 
@@ -21,3 +20,25 @@ func _on_button_command_pressed():
 
 func _on_button_audio_pressed():
 	show_page("PageAudio")
+
+
+# SUBTITLE
+
+# VIDEO
+func _on_resolution_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+
+# VISUAL
+
+# COMMAND
+
+# AUDIO
+func _on_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		AudioServer.get_bus_index("Master"),
+		linear_to_db(value)
+	)

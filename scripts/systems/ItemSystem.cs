c6bf3db36794a2bfee25f624dd9f78f60c21df7c
XEric <eric1.xu@epitech.eu>
2026-01-22 16:55:37 +0100

    feat: add InventorySystem base

---
+++ scripts/systems/ItemSystem.cs
@@ -0,0 +1,20 @@
+﻿namespace AshesOfVelsingrad.systems;
+
+// TODO: Remove it when the Tactical combat branch is merged because it is defined in the skill system
+public enum TargetType
+{
+    None,
+    Allies,
+    Enemies
+}
+
+public abstract class ItemSystem
+{
+    public int Id { get; protected set; }
+    public string Name { get; protected set; }
+    public string Description { get; protected set; }
+    public TargetType TargetType { get; protected set; } = TargetType.None;
+    public string Icon { get; protected set; } = string.Empty;
+    public bool IsStackable { get; protected set; } = false;
+    public int MaxStack { get; protected set; } = 0;
+}

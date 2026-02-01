61299c5fb9af3452319fd2d9c27dbfa39d821cc9
XEric <eric1.xu@epitech.eu>
2026-02-02 00:44:13 +0100

    feat: set burning effect (not tested yet)

---
+++ scripts/systems/DataStructures.cs
@@ -0,0 +1,42 @@
+﻿namespace AshesOfVelsingrad.Systems;
+
+public sealed class DataStructures
+{
+    public enum EffectType
+    {
+        Damage,
+        Heal,
+        Revive,
+        ApplyModifier,
+        RemoveModifier,
+        Control,
+        ResourceChange,
+        Summon
+    }
+
+    public enum StatType
+    {
+        Hp,
+        MaxHp,
+        Atk,
+        Def,
+        Speed,
+        Intelligence,
+        Mana,
+        Curse
+    }
+
+    public enum ModifierType
+    {
+        Flat,
+        Percent
+    }
+
+    public record EffectPayload
+    {
+        public EffectType EffectType;
+        public StatType? StatType;
+        public ModifierType? ModifierType;
+        public float Value;
+    }
+}

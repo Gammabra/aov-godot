7c47b311d5c566527dd934ab77249b89f1bb7d04
XEric <eric1.xu@epitech.eu>
2026-03-29 13:01:06 +0200

    chore: clean plugin.gd file

--- addons/GridMapEditorTool/plugin.gd
+++ addons/GridMapEditorTool/plugin.gd
@@ -23,38 +23,47 @@ func _exit_tree() -> void:
 		popup.queue_free()
 
 func _process(delta: float) -> void:
-	var scene_root = EditorInterface.get_edited_scene_root()
+	var scene_root: Node = EditorInterface.get_edited_scene_root()
 	if root != scene_root:
 		root = scene_root
 		grid_map = Utils.try_find_gridmap(root)
 		hovered_cell = Utils.find_hovered_cell(grid_map)
 
-func _forward_3d_gui_input(camera, event):
+func _forward_3d_gui_input(camera: Camera3D, event: InputEvent):
 	if event is InputEventMouseMotion:
-		if grid_map == null:
-			return
-		var from = camera.project_ray_origin(event.position)
-		var to = from + camera.project_ray_normal(event.position) * 1000
-
-		var space_state = camera.get_world_3d().direct_space_state
-		var result = space_state.intersect_ray(
-			PhysicsRayQueryParameters3D.create(from, to)
-		)
-
-		if result:
-			var cell = grid_map.local_to_map(result.position)
-			current_cell = cell
-			if hovered_cell != null:
-				hovered_cell.set_hovered_cell(cell)
-		else:
-			current_cell = null
-			hovered_cell.name = "No hovered cell"
+		_handle_mouse_hovering(camera, event)
 
 	elif event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-			if current_cell && hovered_cell:
-				_show_popup(current_cell)
-				return true
+			_handle_mouse_button_left_click(event)
+			return true
+
+# --------------------------
+# Forward 3D GUI Input Methods
+# --------------------------
+func _handle_mouse_hovering(camera: Camera3D, event: InputEvent) -> void:
+	if grid_map == null:
+		return
+	var from: Vector3 = camera.project_ray_origin(event.position)
+	var to: Vector3 = from + camera.project_ray_normal(event.position) * 1000
+
+	var space_state: PhysicsDirectSpaceState3D = camera.get_world_3d().direct_space_state
+	var result: Dictionary = space_state.intersect_ray(
+		PhysicsRayQueryParameters3D.create(from, to)
+	)
+
+	if result:
+		var cell: Vector3i = grid_map.local_to_map(result.position)
+		current_cell = cell
+		if hovered_cell != null:
+			hovered_cell.set_hovered_cell(cell)
+	else:
+		current_cell = null
+		hovered_cell.name = "No hovered cell"
+
+func _handle_mouse_button_left_click(event: InputEvent):
+	if current_cell && hovered_cell:
+		_show_popup(current_cell)
 
 # --------------------------
 # POPUP
@@ -67,8 +76,7 @@ func _create_popup():
 	popup.ok_button_text = "Return"
 	popup.exclusive = false
 
-	# Bouton ajouter spawner
-	var add_spawner_btn = Button.new()
+	var add_spawner_btn: Button = Button.new()
 	add_spawner_btn.text = "Add a spawner"
 	add_spawner_btn.pressed.connect(_on_add_spawner_pressed)
 	popup.add_child(add_spawner_btn)
@@ -90,7 +98,7 @@ func _get_or_create_resource() -> CellData:
 	if not scene:
 		return null
 
-	var tres_path = scene.scene_file_path.get_basename() + ".tres"
+	var tres_path: String = scene.scene_file_path.get_basename() + ".tres"
 	var res: CellData
 	if ResourceLoader.exists(tres_path):
 		res = load(tres_path)
@@ -100,7 +108,7 @@ func _get_or_create_resource() -> CellData:
 	return res
 
 func _on_add_spawner_pressed():
-	var res = _get_or_create_resource()
+	var res: CellData = _get_or_create_resource()
 	if res:
 		res.add_player_spawner(current_cell)
 		ResourceSaver.save(res, get_tree().edited_scene_root.scene_file_path.get_basename() + ".tres")

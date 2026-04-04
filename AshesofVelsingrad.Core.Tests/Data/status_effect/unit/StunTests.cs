d8bc247478b783387173065a2dc0cba717efe06f
Louis Ferrari <louis.ferrari@epitech.eu>
2026-04-04 16:54:41 +0300

    test(combat): :white_check_mark: add unit tests for Stun

---
+++ AshesofVelsingrad.Core.Tests/Data/status_effect/unit/StunTests.cs
@@ -0,0 +1,79 @@
+using NUnit.Framework;
+using Moq;
+using AshesOfVelsingrad.Data;
+using AshesOfVelsingrad.Systems;
+
+namespace AshesOfVelsingrad.Core.Tests.Data;
+
+[TestFixture]
+public class StunTests
+{
+    private Mock<IUnitSystem>? _mockUnit;
+    private const int _duration = 1;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockUnit = new Mock<IUnitSystem>();
+    }
+
+    [Test]
+    public void OnApply_CallsUnitOnEffectControlApplied()
+    {
+        if (_mockUnit == null)
+        {
+            Assert.Fail("MockUnit not initialized properly.");
+            return;
+        }
+
+        // Arrange
+        var stun = new Stun(_duration);
+
+        // Act
+        stun.OnApply(_mockUnit.Object);
+
+        // Assert
+        _mockUnit.Verify(u => u.OnEffectControlApplied(), Times.Once);
+    }
+
+    [Test]
+    public void OnRemove_CallsUnitOnEffectControlRemoved()
+    {
+        if (_mockUnit == null)
+        {
+            Assert.Fail("MockUnit not initialized properly.");
+            return;
+        }
+
+        // Arrange
+        var stun = new Stun(_duration);
+
+        // Act
+        stun.OnRemove(_mockUnit.Object);
+
+        // Assert
+        _mockUnit.Verify(u => u.OnEffectControlRemoved(), Times.Once);
+    }
+
+    [Test]
+    public void ShouldApplyTwice_ReturnsTrue()
+    {
+        // Arrange
+        var stun = new Stun(_duration);
+
+        // Assert
+        Assert.That(stun.ShouldApplyTwice, Is.True);
+    }
+
+    [Test]
+    public void Constructor_SetsInheritedPropertiesCorrectly()
+    {
+        // Act
+        var stun = new Stun(_duration);
+
+        // Assert
+        Assert.That(stun.Name, Is.EqualTo("Stun"));
+        Assert.That(stun.Duration, Is.EqualTo(_duration));
+        // Note: Amount and ModifierType aren't used here, but they exist in the base
+    }
+}

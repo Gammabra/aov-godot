9ab6907ea2068cb0c6f4ef9cb60c6404a48cd905
Louis Ferrari <louis.ferrari@epitech.eu>
2026-04-06 22:29:35 +0300

    test(level): :white_check_mark: reach 100% coverage on CellInformation

--- AshesofVelsingrad.Core.Tests/Systems/map_system/CellInformationTests.cs
+++ AshesofVelsingrad.Core.Tests/Systems/map_system/CellInformationTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using Moq;
 using AshesOfVelsingrad.Systems;
+using System.Reflection;
 using AshesOfVelsingrad.Utilities;
 
 namespace AshesOfVelsingrad.Core.Tests.Systems;
@@ -12,6 +13,16 @@ public class CellInformationTests
     private const int _y = 2;
     private const int _z = 3;
     private const AovDataStructures.CellType _defaultTerrain = AovDataStructures.CellType.Grass;
+
+    public class TestStatusEffect<T> : StatusEffect<T>
+    {
+        public TestStatusEffect(
+            string name, 
+            int duration, 
+            AovDataStructures.ModifierType modType, 
+            float amount
+        ) : base(name, "desc", duration, false, modType, amount) { }
+    }
 
     [Test]
     public void Constructor_InitializesPropertiesCorrectly()
@@ -61,5 +72,64 @@ public class CellInformationTests
         // Clear unit
         cell.SetUnit(null);
         Assert.That(cell.Unit, Is.Null);
+    }
+
+    [Test]
+    public void OnUnitEntered_WhenCellHasSpreadableEffect_AppliesToUnit()
+    {
+        // Arrange
+        var cell = new CellInformation(_x, _y, _z, _defaultTerrain, true);
+        var mockUnit = new Mock<IUnitSystem>();
+        
+        // 1. Create a concrete instance of our stub
+        var unitEffect = new TestStatusEffect<IUnitSystem>("Burning", 3, AovDataStructures.ModifierType.Flat, 5f);
+        var cellEffect = new TestStatusEffect<CellInformation>("FireTile", 3, AovDataStructures.ModifierType.Flat, 0f);
+
+        // 2. REFLECTION: Force the value into the init-only property
+        // We look for the backing field (usually named '<EffectToSpread>k__BackingField')
+        var field = typeof(StatusEffect<CellInformation>)
+            .GetField("<EffectToSpread>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        if (field != null)
+        {
+            field.SetValue(cellEffect, unitEffect);
+        }
+        else
+        {
+            // Fallback: Try setting via the property if the backing field name differs
+            var prop = typeof(StatusEffect<CellInformation>).GetProperty("EffectToSpread");
+            prop?.SetValue(cellEffect, unitEffect);
+        }
+
+        cell.ApplyEffect(cellEffect);
+
+        // Act
+        cell.OnUnitEntered(mockUnit.Object);
+
+        // Assert
+        // If reflection worked, Line 42 in CellInformation will now see the effect!
+        mockUnit.Verify(u => u.SetStatusEffectOnUnit(unitEffect), Times.Once);
+    }
+
+    [Test]
+    public void OnUnitEntered_WhenCellHasNoSpreadableEffect_DoesNothing()
+    {
+        // Arrange
+        var cell = new CellInformation(_x, _y, _z, _defaultTerrain, true);
+        var mockUnit = new Mock<IUnitSystem>();
+        
+        var nonSpreadingEffect = new TestStatusEffect<CellInformation>(
+            "Static", 3,
+            AovDataStructures.ModifierType.Flat, 0
+        );
+        // EffectToSpread is null by default
+        
+        cell.ApplyEffect(nonSpreadingEffect);
+
+        // Act
+        cell.OnUnitEntered(mockUnit.Object);
+
+        // Assert
+        mockUnit.Verify(u => u.SetStatusEffectOnUnit(It.IsAny<StatusEffect<IUnitSystem>>()), Times.Never);
     }
 }

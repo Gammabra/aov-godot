cc670e49c381abaefd8559c66610fa2811f5123c
XEric <eric1.xu@epitech.eu>
2026-05-02 21:19:22 +0200

    feat: add spring arm handler

---
+++ AshesofVelsingrad/scripts/player/ExplorationCamera.cs
@@ -0,0 +1,27 @@
+﻿using Godot;
+
+namespace AshesOfVelsingrad.player;
+
+public partial class ExplorationCamera : SpringArm3D
+{
+    private float _mouseSensitivity = 0.1f;
+
+    public override void _Ready()
+    {
+        Input.SetMouseMode(Input.MouseModeEnum.Captured);
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion mouseMotion)
+        {
+            Vector3 rotation = RotationDegrees;
+            rotation.X -= mouseMotion.Relative.Y * _mouseSensitivity;
+            rotation.X = Mathf.Clamp(rotation.X, -40, 40);
+
+            rotation.Y -= mouseMotion.Relative.X * _mouseSensitivity;
+            rotation.Y = Mathf.Wrap(rotation.Y, 0, 360);
+            RotationDegrees = rotation;
+        }
+    }
+}

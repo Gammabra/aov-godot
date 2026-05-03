f77f1af5504713b9a61c6b03523b9c8efdab2eed
XEric <eric1.xu@epitech.eu>
2026-05-03 23:11:29 +0200

    chore: add needed files

---
+++ AshesofVelsingrad/scripts/player/States/IdleState.cs
@@ -0,0 +1,27 @@
+using AshesOfVelsingrad.Managers;
+using Godot;
+
+namespace AshesOfVelsingrad.player.States;
+
+public partial class IdleState : State
+{
+	[Export]
+	private NodePath? _animatedSprite3DPath;
+
+	private AnimatedSprite3D? _animatedSprite3D;
+
+	public override void OnStateReady()
+	{
+		_animatedSprite3D = GetNode<AnimatedSprite3D>(_animatedSprite3DPath);
+	}
+
+	public override void Enter()
+	{
+		_animatedSprite3D?.Play("Idle");
+	}
+
+	public override void Exit()
+	{
+		_animatedSprite3D?.Stop();
+	}
+}
---
+++ AshesofVelsingrad/scripts/player/States/WalkForwardState.cs
@@ -0,0 +1,27 @@
+﻿using Godot;
+using AshesOfVelsingrad.Managers;
+
+namespace AshesOfVelsingrad.player.States;
+
+public partial class WalkForwardState : State
+{
+    [Export]
+    private NodePath? _animatedSprite3DPath;
+
+    private AnimatedSprite3D? _animatedSprite3D;
+
+    public override void OnStateReady()
+    {
+        _animatedSprite3D = GetNode<AnimatedSprite3D>(_animatedSprite3DPath);
+    }
+
+    public override void Enter()
+    {
+        _animatedSprite3D?.Play("WalkForward");
+    }
+
+    public override void Exit()
+    {
+        _animatedSprite3D?.Stop();
+    }
+}

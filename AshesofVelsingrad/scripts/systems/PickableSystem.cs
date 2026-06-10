3731b640cdbbde86c5b1e6cf839d47d4ac849e59
XEric <eric1.xu@epitech.eu>
2026-06-10 21:05:40 +0200

    docs: add doxygen to the PickableSystem class

--- AshesofVelsingrad/scripts/systems/PickableSystem.cs
+++ AshesofVelsingrad/scripts/systems/PickableSystem.cs
@@ -1,12 +1,17 @@
 ﻿using AshesOfVelsingrad.Managers;
 using AshesOfVelsingrad.Systems;
-using AshesOfVelsingrad.systems.skills.behaviours;
-using Godot;
 
 namespace AshesofVelsingrad.scripts.systems;
 
+/// <summary>
+/// Handles interactions with pickable items.
+/// </summary>
 public static class PickableSystem
 {
+    /// <summary>
+    /// Adds the specified item to the player's inventory and removes it from the world if successful.
+    /// </summary>
+    /// <param name="itemSystem">The item to pick up.</param>
     public static void AddToInventory(ItemSystem itemSystem)
     {
         if (PlayerInventoryManager.Instance is { } inv)

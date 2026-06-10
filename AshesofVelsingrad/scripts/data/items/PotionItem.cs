7931aa69f211a29fef2fb08bfef39d1485665e02
XEric <eric1.xu@epitech.eu>
2026-06-10 20:51:04 +0200

    feat: pickup system works

--- AshesofVelsingrad/scripts/data/items/PotionItem.cs
+++ AshesofVelsingrad/scripts/data/items/PotionItem.cs
@@ -1,11 +1,17 @@
+using Godot;
 using AshesOfVelsingrad.Systems;
 using AshesOfVelsingrad.Utilities;
 
 namespace AshesOfVelsingrad.items;
 
-public sealed class PotionItem : ItemSystem
+public sealed partial class PotionItem : ItemSystem
 {
-    public PotionItem()
+    [Export]
+    private NodePath? _interactTextPath;
+
+    private Label3D? _interactText;
+
+    public override void _Ready()
     {
         Id = 1;
         Name = "Potion";
@@ -14,11 +20,25 @@ public sealed class PotionItem : ItemSystem
         IsStackable = true;
         MaxStack = 10;
         TargetType = AovDataStructures.TargetTypes.SingleAlly;
+        _interactText = GetNode<Label3D>(_interactTextPath);
+        ItemCatalog.Register(this);
     }
 
     public override void Use(IUnitSystem user, IUnitSystem? target, IMapSystem? map)
     {
         if (target == null)
             return;
+    }
+
+    public override void ShowPrompt()
+    {
+        if (_interactText is not null)
+            _interactText.Visible = true;
+    }
+
+    public override void HidePrompt()
+    {
+        if (_interactText is not null)
+            _interactText.Visible = false;
     }
 }

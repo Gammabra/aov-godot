13d834eb2f54d1c3258038912ffcb87a45ff5b2f
XEric <eric1.xu@epitech.eu>
2026-06-23 01:27:21 +0200

    feat: add an item detection area for the first item to make the mercenary go to it

---
+++ AshesofVelsingrad/scripts/tutorial/ItemDetectionArea.cs
@@ -0,0 +1,36 @@
+using AshesOfVelsingrad.Interfaces;
+using Godot;
+
+namespace AshesOfVelsingrad.tutotial;
+
+public partial class ItemDetectionArea : Area3D
+{
+	[Export]
+	private NodePath _characterThatDetectPath;
+
+	[Export]
+	private NodePath _pointOfInterestPath;
+
+	private CharacterBody3D? _characterThatDetect;
+	private Marker3D? _pointOfInterest;
+
+	private async void OnBodyEntered(Node3D body)
+	{
+		if (body == _characterThatDetect &&
+			_pointOfInterest != null &&
+			body is NpcSystem npc)
+		{
+			npc.ToIdle(true);
+			await ToSignal(GetTree().CreateTimer(1),
+				SceneTreeTimer.SignalName.Timeout);
+			npc.ToFollowingSpecificPoint(_pointOfInterest);
+		}
+	}
+
+	public override void _Ready()
+	{
+		_characterThatDetect = GetNode<CharacterBody3D>(_characterThatDetectPath);
+		_pointOfInterest = GetNode<Marker3D>(_pointOfInterestPath);
+		BodyEntered += OnBodyEntered;
+	}
+}

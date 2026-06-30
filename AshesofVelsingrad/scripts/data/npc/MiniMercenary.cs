d0f8dcc87b97c66cc461a171316925535cb64c45
XEric <eric1.xu@epitech.eu>
2026-06-30 18:32:15 +0200

    feat: add a noticed sprite on the mini mercenary when he detected the first item

--- AshesofVelsingrad/scripts/data/npc/MiniMercenary.cs
+++ AshesofVelsingrad/scripts/data/npc/MiniMercenary.cs
@@ -19,15 +19,20 @@ public partial class MiniMercenary : NpcSystem
     [Export]
     private float _speed = 4.4f;
 
+    [Export]
+    private NodePath _noticedPath = null!;
+
     private NavigationAgent3D _navigationAgent = null!;
     private AovPlayer? _player;
     private StateMachine _stateMachine = null!;
+    private Sprite3D _noticed = null!;
 
     public override void _Ready()
     {
         NavigationAgent3D navigationAgent = GetNode<NavigationAgent3D>(_navigationAgentPath);
-        _player = GetNode<AovPlayer>(_playerPath);
         StateMachine stateMachine = GetNode<StateMachine>(_stateMachinePath);
+        _player = GetNode<AovPlayer>(_playerPath);
+        _noticed = GetNode<Sprite3D>(_noticedPath);
         Initialize(stateMachine, navigationAgent, _speed);
         StopDistance = 1.5f;
         ToFollowingMovingEntity(_player);
@@ -41,5 +46,15 @@ public partial class MiniMercenary : NpcSystem
         }
 
         HandleCharacterMovement(delta);
+    }
+
+    public void ToggleNoticed()
+    {
+        if (_noticed.Visible)
+        {
+            _noticed.Visible = false;
+            return;
+        }
+        _noticed.Visible = true;
     }
 }

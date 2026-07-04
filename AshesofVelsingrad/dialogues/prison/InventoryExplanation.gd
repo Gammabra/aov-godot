0e6619ffff84d94a366458f6667e4cafda108d4c
Anaëlle URBON <anaelle.urbon@live.fr>
2026-07-04 16:56:14 +0200

    fix: Best dialogue for the interaction and inventory tutorial

--- AshesofVelsingrad/dialogues/prison/InventoryExplanation.gd
+++ AshesofVelsingrad/dialogues/prison/InventoryExplanation.gd
@@ -5,9 +5,14 @@ signal dialog_ended()
 var dio=Dialog.new()
 var dialog:=dio.start(self)
 
+var narrateur = dialog.Character(
+		"",
+		Color("ebede9"),
+		" "
+	)
 var mercenary = dialog.Character(
-		"Mercenary",
-		Color.YELLOW_GREEN,
+		"Arthur",
+		Color("be772b"),
 		"res://assets/Krita/icone_mercenaire.png"
 	)
 
@@ -15,19 +20,20 @@ func _ready() -> void:
 	dialog.typewriter_speed=30
 
 func first():
-	dialog.say("Good! You'll find it in your inventory.", mercenary)
-	dialog.say("You can open it with the (I) keybind.", mercenary)
+	dialog.say("Bien. Garde ça près de toi, on ne sait pas ce qui nous attend.", mercenary)
+	dialog.say("Les objets ramassés sont rangés dans ton inventaire. Appuie sur I pour l'ouvrir.", narrateur)
 
 	dialog.action("_emit_dialog_end")
 	dialog.start_convo()
 
 func second():
-	dialog.say("On the left, you'll find your exploration inventory. Any items you pick up will appear here.", mercenary)
-	dialog.say("On the right, you'll see the personal inventory of each party member.", mercenary)
-	dialog.say("This will be very useful during battles, as items can only be used during a party member's turn.", mercenary)
-	dialog.say("You can equip a party member simply by dragging an item into one of their equipment slots.", mercenary)
-	dialog.say("Remember, you won't have access to the exploration inventory during battle, so make sure everyone is properly equipped before heading into combat.", mercenary)
-	dialog.say("That's all. I think you're ready now.", mercenary)
+	dialog.say("À gauche se trouve l'inventaire d'exploration. Les objets ramassés pendant ton voyage y apparaîtront.", narrateur)
+	dialog.say("À droite se trouve l'inventaire personnel de chaque membre du groupe.", narrateur)
+	dialog.say("En combat, les objets ne peuvent être utilisés que pendant le tour du personnage qui les porte.", narrateur)
+	dialog.say("Pour équiper un personnage, fais glisser un objet vers l'un de ses emplacements d'équipement.", narrateur)
+	dialog.say("L'inventaire d'exploration n'est pas accessible pendant les combats. Prépare ton groupe avant d'engager l'ennemi.", narrateur)
+	dialog.say("Tout est en ordre ?", mercenary)
+	dialog.say("Alors on avance.", mercenary)
 
 	dialog.action("_emit_dialog_end")
 	dialog.start_convo()
